184996e733dfd37aec94b87ce5fa6a51273199b1
Teacher <teacher@ac1mwk18gjc9h.78b505.nycboe.org>
2021-09-27 08:18:08 -0400

    week 2 work

---
+++ 01-perfect-day/PerfectDay.java
@@ -0,0 +1,30 @@
+public class PerfectDay {
+  public static void main(String[] args) {
+    System.out.println("Here's my perfect day:");
+    morning();
+
+  } // end main method
+
+  public static void morning() {
+    System.out.print("In the morning, ");
+    System.out.println("I would sleep late and cook a big breakfast.");
+    System.out.println("I will go on a 10 mile bike ride.");
+  } // end morning method
+  
+} // end class
+
+// in the morning i would sleep until 9:00
+// i would cook a big breakfast for the family; eggs, potatoes, sausage
+// i would go on a 10 mile bike ride
+
+/*
+in the afternoon,
+i would go on a nature walk
+with my family in prospect park
+we would get ice cream afterwards
+*/
+
+/*
+at night, i would watch the mets game
+after bbq-ing some burgers
+*/
